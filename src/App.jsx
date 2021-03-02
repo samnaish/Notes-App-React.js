446b050b7ecfc0419bccc1561d2cb22d53b96ced
repl.it user <replituser@example.com>
2021-03-02 08:55:42 +0000

    feat: can change note text and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,61 @@
-import React, { useState } from 'react';
-import uuid from 'react-uuid'
-import './App.css';
-
-import Sidebar from './components/Sidebar'
-import Mainbar from './components/Mainbar'
+import React, { useEffect, useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
+import Main from "./components/Mainbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-	const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setActiveNote] = useState(false);
 
-	const onAddNote = () => {
-		console.log('added');
-		const newNote = {
-			id: uuid(),
-			title: "untitled note",
-			body: "",
-			lastModified: Date.now(),
-		}
-		setNotes([newNote,...notes]);
-	}
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+  };
+
+  const onDeleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArr);
+  };
+
+  const getActiveNote = () => {
+    return notes.find(({ id }) => id === activeNote);
+  };
 
   return (
-    <div className="app__container">
-			<Sidebar notes={notes} onAddNote={onAddNote}/>
-			<Mainbar/>
+    <div className="App">
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from 'react'
 
-const Sidebar = ({ notes, onAddNote }) => {
-   return (
-      <div className="app__sidebar">
-				<div className="app__sidebar-header">
-					<h1>Notes</h1>
-					<button onClick={onAddNote}>Add</button>
-				</div>
-				<div className="app__sidebar-notes">
-				{
-					notes.map((note) => {
-						return (
-							<div key={note.id} className="app__sidebar-note">
-								<div className="sidebar__note-title">
-									<strong>{note.title}</strong>
-									<button>Delete</button>
-								</div>
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
 
-								<p>{note.body && note.body.substr(0, 100) + "..."}</p>
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+          <div
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button onClick={(e) => onDeleteNote(id)}>Delete</button>
+            </div>
 
-								<small className="note-meta">Last Modified {new Date(note.lastModified).toLocaleDateString("en-GB", {})}</small>
-							</div>
+            <p>{body && body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+              Last Modified{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-						)
-					})
-				}
-				</div>
-			</div>
-   )
-}
 export default Sidebar;
